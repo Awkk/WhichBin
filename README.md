3d535e2580f0a3025a2ea542c46fbdf6295f4545
Andrew Wong <kkwong125@gmail.com>
2019-03-08 00:57:45 -0800

    Created animation for transition between Home and Recyclable.

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
 # WhichBin
-
-i edit it
--- index.js
+++ index.js
@@ -1,44 +1,42 @@
-// Set click event listener for all nav items
-$("nav a").on("click", function () {
-  navItemActive(this);
+/*************************** Navbar functions ******************************/
 
-  switch (this.id) {
-    case "nav-logo":
-      homePageChage();
-      break;
-    case "nav-home":
-      homePageChage();
-      break;
-    case "nav-recyclable":
-      recyclablePageChage();
-      break;
-  }
-});
+// Set click event listener for nav elements
+$("#nav-logo").on("click", function () {
+  if (!$(this).hasClass("active"))
+    homePageChage();
 
-// Set class .active to nav-item that is selected
+  navItemActive($(this));
+})
+$("#nav-home").on("click", function () {
+  if (!$(this).hasClass("active"))
+    homePageChage();
+
+  navItemActive($(this));
+})
+$("#nav-recyclable").on("click", function () {
+  if (!$(this).hasClass("active"))
+    recyclablePageChage();
+
+  navItemActive($(this));
+})
+
+// Set class .active to nav element that is selected
 function navItemActive(navItem) {
   $(".active").removeClass("active");
 
-  if (navItem.id == "nav-logo") {
-    $("#nav-home")[0].setAttribute("class", "active");
+  if (navItem.attr("id") == "nav-logo") {
+    $("#nav-home").addClass("active");
   } else {
-    navItem.setAttribute("class", "active");
+    navItem.addClass("active");
   }
 };
 
-mysteryItemHoverListener()
+/*************************** mysteryItem functions ******************************/
 
-function recyclablePageChage() {
-  $("#mystery-item").off("mouseenter mouseleave");
-  revealMysteryItem();
-};
+// Set listener for mystery-item
+mysteryItemHoverListener();
 
-function homePageChage() {
-  mysteryItemHoverListener()
-  hideMysteryItem();
-};
-
-// Reveal mystery-item when mouse over, backout again when mouse out
+// Listener for revealing mystery-item when mouse over, backout again when mouse out
 function mysteryItemHoverListener() {
   $("#mystery-item").hover(function () {
     revealMysteryItem();
@@ -48,7 +46,7 @@ function mysteryItemHoverListener() {
 };
 
 function revealMysteryItem() {
-  $("#mystery-item").css("animation", "reveal-item 2s");
+  $("#mystery-item").css("animation", "reveal-item 1s");
   $("#mystery-item").css("animation-fill-mode", "forwards");
   $("#question-mark").hide();
 };
@@ -58,3 +56,87 @@ function hideMysteryItem() {
   $("#mystery-item").css("animation", "none");
   $("#question-mark").show();
 };
+
+/******************** Home and Recyclable page switching functions *******************/
+
+// Change the layout to recyclable page
+function recyclablePageChage() {
+  $("#mystery-item").off("mouseenter mouseleave");
+  revealMysteryItem();
+  moveMysteryItem_recyclable();
+  moveSearchBar_recyclable();
+  moveBins_recyclable();
+};
+
+// Change the layout to home page
+function homePageChage() {
+  mysteryItemHoverListener();
+  hideMysteryItem();
+  moveSearchBar_home();
+  moveMysteryItem_home();
+  moveBins_home()
+};
+
+/**************** Home and Recyclable page switching functions animations ****************/
+
+function moveSearchBar_home() {
+  var searchBar = $("#search-container");
+  var offset = searchBar.offset();
+
+  searchBar.animate({
+    top: "0",
+    left: "0",
+    zoom: "1",
+    width: "50%",
+  }, 600)
+};
+
+function moveSearchBar_recyclable() {
+  var searchBar = $("#search-container");
+  var offset = searchBar.offset();
+
+  searchBar.animate({
+    top: "+=" + ($("nav").height() * 0.6 - offset.top),
+    left: "+=" + (($(window).width() - $("#page-wrapper").width()) / 2 - offset.left - searchBar.width() / 1.8),
+    zoom: "0.7",
+    width: "20%",
+  }, 600)
+};
+
+function moveMysteryItem_home() {
+  var mysteryItem = $("#mystery-item-container");
+  var offset = mysteryItem.offset();
+
+  mysteryItem.animate({
+    left: "0"
+  }, 600)
+};
+
+function moveMysteryItem_recyclable() {
+  var mysteryItem = $("#mystery-item-container");
+  var offset = mysteryItem.offset();
+
+  mysteryItem.animate({
+    left: "+=" + $("#page-wrapper").width() * 0.1
+  }, 600)
+};
+
+function moveBins_home() {
+  var bins = $("#bins-container");
+  var offset = bins.offset();
+
+  bins.animate({
+    left: "0",
+    top: "0"
+  }, 600)
+}
+
+function moveBins_recyclable() {
+  var bins = $("#bins-container");
+  var offset = bins.offset();
+
+  bins.animate({
+    left: "+=" + $("#page-wrapper").width() * 0.1,
+    top: "+=" + ($("#page-wrapper").height() - offset.top - bins.height() - 70)
+  }, 600)
+}
