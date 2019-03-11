c9a56398b262957c4000f039a73ce0f6ed24ae8a
Andrew Wong <kkwong125@gmail.com>
2019-03-11 01:14:12 -0700

    Modefied animation on item showcase

--- index.js
+++ index.js
@@ -19,6 +19,9 @@ $("#nav-recyclable").on("click", function () {
 
   navItemActive($(this));
 })
+$("#nav-play").on("click", function () {
+  navItemActive($(this));
+})
 
 // Set class .active to nav element that is selected
 function navItemActive(navItem) {
@@ -30,98 +33,6 @@ function navItemActive(navItem) {
     navItem.addClass("active");
   }
 };
-
-/*************************** mysteryItem functions ******************************/
-
-var scrollingWrapper = $("#scrolling-wrapper");
-let itemWidth = $(".item").width();
-var allItems = $(".mystery-item");
-let middleOffset = 80;
-var middleItemPosition = itemWidth * 2 + middleOffset;
-var middleItemIndex = 3;
-
-// Move items to the forth one as default
-scrollingWrapper.scrollLeft(middleItemPosition);
-itemsRevealAndHide();
-
-// Set click listener on left and right arrow
-$("#left-arrow").on("click", function () {
-  moveToPerviousItem();
-});
-
-$("#right-arrow").on("click", function () {
-  moveToNextItem();
-});
-
-function moveToPerviousItem() {
-  middleItemIndex -= 1;
-  if (middleItemIndex == -2) {
-    middleItemIndex = allItems.length - 2;
-  };
-  itemsRevealAndHide();
-  lastItemToFirst();
-}
-
-function moveToNextItem() {
-  middleItemIndex += 1;
-  if (middleItemIndex == allItems.length) {
-    middleItemIndex = 0;
-  };
-  itemsRevealAndHide();
-  firstItemToLast();
-}
-
-function itemsRevealAndHide() {
-  var previousItemIndex = middleItemIndex - 1
-  var nextItemIndex = middleItemIndex + 1
-
-  if (previousItemIndex == -1) {
-    previousItemIndex = allItems.length - 1;
-  }
-  if (nextItemIndex == allItems.length) {
-    nextItemIndex = 0;
-  }
-
-  allItems.eq(previousItemIndex).css({
-    "animation": "none",
-    "filter": "brightness(15%)"
-  }).animate({
-    "width": "70%"
-  }, 100);
-  allItems.eq(middleItemIndex).css({
-    "animation": "reveal-item 1s",
-    "animation-fill-mode": "forwards"
-  }).animate({
-    "width": "100%"
-  }, 100);
-  allItems.eq(nextItemIndex).css({
-    "animation": "none",
-    "filter": "brightness(15%)"
-  }).animate({
-    "width": "70%"
-  }, 100);
-}
-
-function lastItemToFirst() {
-  var lastItem = $(".item").last().detach();
-  lastItem.css("width", "0");
-  scrollingWrapper.prepend(lastItem);
-  lastItem.animate({
-    width: "220"
-  }, 250);
-}
-
-function firstItemToLast() {
-  var firstItem = $(".item").first();
-
-  firstItem.animate({
-    width: "0"
-  }, 250, function () {
-    firstItem.detach();
-    firstItem.css("width", "220");
-    scrollingWrapper.append(firstItem);
-  });
-}
 
 /******************** Home and Recyclable page switching functions *******************/
 
@@ -141,10 +52,13 @@ function homePageChage() {
 
 /**************** Home and Recyclable page switching functions animations ****************/
 
-function moveSearchBar_home() {
-  var searchBar = $("#search-container");
+var $searchBar = $("#search-container");
+var $mysteryItemsWrapper = $("#mystery-item-wrapper");
+var $bins = $("#bins-container");
+var $midItemOffset;
 
-  searchBar.animate({
+function moveSearchBar_home() {
+  $searchBar.animate({
     top: "0",
     left: "0",
     zoom: "1",
@@ -153,51 +67,153 @@ function moveSearchBar_home() {
 };
 
 function moveSearchBar_recyclable() {
-  var searchBar = $("#search-container");
-  var offset = searchBar.offset();
+  var offset = $searchBar.offset();
 
-  searchBar.animate({
+  $searchBar.animate({
     top: "+=" + ($("nav").height() * 0.6 - offset.top),
-    left: "+=" + (($(window).width() - $("#page-wrapper").width()) / 2 - offset.left - searchBar.width() / 1.8),
+    left: "+=" + (($(window).width() - $("#page-wrapper").width()) / 2 - offset.left - $searchBar.width() / 1.8),
     zoom: "0.7",
     width: "20%",
   }, 600)
 };
 
-function moveMysteryItem_home() {
-  var mysteryItem = $("#mystery-item-container");
-  var offset = mysteryItem.offset();
 
-  mysteryItem.animate({
-    left: "0"
-  }, 600)
+function moveMysteryItem_home() {
+  var $item = $("#item").fadeOut(400, function () {
+    $item.remove();
+    $mysteryItemsWrapper.fadeIn(300);
+  });
+
 };
 
 function moveMysteryItem_recyclable() {
-  var mysteryItem = $("#mystery-item-container");
-  var offset = mysteryItem.offset();
+  $midItemOffset = $allMysteryItems.eq(midItemIndex).offset();
+  var $item = $allMysteryItems.eq(midItemIndex).clone();
+  $item.attr("id", "item");
+  $item.css({
+    "position": "absolute",
+    "width": "220px",
+    "animation": "none",
+    "filter": "none",
+    "left": $midItemOffset.left - ($(window).width() - $("#page-wrapper").width()) / 2
+  })
+  $("#content-wrapper").prepend($item);
 
-  mysteryItem.animate({
-    left: "+=" + $("#page-wrapper").width() * 0.1
-  }, 600)
+  $mysteryItemsWrapper.fadeOut("100", function () {
+    $item.css({
+      "position": "relative",
+      "left": "0"
+    });
+    $item.animate({
+      left: "+=" + $("#page-wrapper").width() * 0.1
+    }, 600)
+  });
 };
 
 function moveBins_home() {
-  var bins = $("#bins-container");
-  var offset = bins.offset();
-
-  bins.animate({
+  $bins.animate({
     left: "0",
     top: "0"
   }, 600)
 }
 
 function moveBins_recyclable() {
-  var bins = $("#bins-container");
-  var offset = bins.offset();
+  var offset = $bins.offset();
 
-  bins.animate({
+  $bins.animate({
     left: "+=" + $("#page-wrapper").width() * 0.1,
-    top: "+=" + ($("#page-wrapper").height() - offset.top - bins.height() - 70)
+    top: "+=" + ($("#page-wrapper").height() - offset.top - $bins.height() - 70)
   }, 600)
+}
+
+/*************************** mysteryItem functions ******************************/
+
+var $scrollingWrapper = $("#scrolling-wrapper");
+var $allMysteryItems = $(".mystery-item");
+const itemWidth = $(".item").width();
+const middleOffset = 58;
+var midItemIndex = 3;
+
+// Move items to the forth one as default
+$scrollingWrapper.scrollLeft(itemWidth * 2 + middleOffset);
+itemsRevealAndHide();
+
+// Set click listener on left and right arrow
+$("#left-arrow").on("click", function () {
+  moveToPerviousItem();
+});
+
+$("#right-arrow").on("click", function () {
+  moveToNextItem();
+});
+
+function moveToPerviousItem() {
+  midItemIndex -= 1;
+  if (midItemIndex == -2) {
+    midItemIndex = $allMysteryItems.length - 2;
+  };
+  itemsRevealAndHide();
+  lastItemToFirst();
+}
+
+function moveToNextItem() {
+  midItemIndex += 1;
+  if (midItemIndex == $allMysteryItems.length) {
+    midItemIndex = 0;
+  };
+  itemsRevealAndHide();
+  firstItemToLast();
+}
+
+function itemsRevealAndHide() {
+  var previousItemIndex = midItemIndex - 1
+  var nextItemIndex = midItemIndex + 1
+
+  if (previousItemIndex == -1) {
+    previousItemIndex = $allMysteryItems.length - 1;
+  }
+  if (nextItemIndex == $allMysteryItems.length) {
+    nextItemIndex = 0;
+  }
+
+  $allMysteryItems.eq(previousItemIndex).css({
+    "animation": "none",
+    "filter": "brightness(12%)"
+  }).animate({
+    "width": "60%"
+  }, 250);
+  $allMysteryItems.eq(midItemIndex).css({
+    "animation": "reveal-item 1s",
+    "animation-fill-mode": "forwards"
+  }).animate({
+    "width": "100%"
+  }, 250);
+  $allMysteryItems.eq(nextItemIndex).css({
+    "animation": "none",
+    "filter": "brightness(12%)"
+  }).animate({
+    "width": "60%"
+  }, 250);
+}
+
+function lastItemToFirst() {
+  var $lastItem = $(".item").last().detach();
+
+  $lastItem.css("width", "0");
+  $scrollingWrapper.prepend($lastItem);
+  $lastItem.animate({
+    width: "220"
+  }, 250);
+}
+
+function firstItemToLast() {
+  var $firstItem = $(".item").first();
+
+  $firstItem.animate({
+    width: "0"
+  }, 250, function () {
+    $firstItem.detach();
+    $firstItem.css("width", "220");
+    $scrollingWrapper.append($firstItem);
+  });
 }
